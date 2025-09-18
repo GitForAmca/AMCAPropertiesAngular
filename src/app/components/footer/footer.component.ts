c70d40fca57e551167c84808dc0227deee19fdb7
It7amca <it7@amca.ae>
2025-09-18 17:20:02 +0400

    I updated content of 8 pages and I implemented Reactive form validation on subscribe form in footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators,ReactiveFormsModule } from '@angular/forms';
 import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule, RouterLink],
+  imports: [RouterModule, RouterLink,ReactiveFormsModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-  email : string = "info@amcaproperties.com"
+   email : string = "info@amcaproperties.com"
+
+    subscribeForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+   
+      email: new FormControl('', [Validators.required, Validators.email])
+  })
+  formValue: any;
+  onSave(){
+    this.formValue = this.subscribeForm.value;
+  }
 }
+
