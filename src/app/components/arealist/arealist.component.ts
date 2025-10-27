4f5215fc98af053e00c677eb58c68e157a85d562
It7amca <it7@amca.ae>
2025-10-27 14:05:39 +0400

    add loader on the community page

--- src/app/components/arealist/arealist.component.ts
+++ src/app/components/arealist/arealist.component.ts
@@ -4,35 +4,41 @@ import { Router, RouterLink } from '@angular/router';
 import { IAreaGuide } from '../../model/interface/IAreaGuide';
 import { AreaGuide } from '../../model/class/AreaGuide';
 import { CommonModule } from '@angular/common';
+import { SkeletonloaderComponent } from '../../reusableComponent/skeletonloader/skeletonloader.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-arealist',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, SkeletonloaderComponent],
   templateUrl: './arealist.component.html',
-  styleUrl: './arealist.component.scss'
+  styleUrl: './arealist.component.scss',
 })
 export class ArealistComponent {
   constructor(
-    private areaguideservice : AreaguideService,
-    private router : Router
-  ){}
+    private areaguideservice: AreaguideService,
+    private router: Router
+  ) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.GetAreaGuide();
   }
 
-  AreaGuideInterface : IAreaGuide[] = [];
-  AreaGuideObj : AreaGuide = new AreaGuide();
+  AreaGuideInterface: IAreaGuide[] = [];
+  AreaGuideObj: AreaGuide = new AreaGuide();
+  skeletonArray = Array(3);
+  IsLoaded: boolean = false;
 
-
-  GetAreaGuide(){
-    this.areaguideservice.GetAreaGuide(this.AreaGuideObj).subscribe((result : any) =>{
-      this.AreaGuideInterface = result;
-    })
+  GetAreaGuide() {
+    this.areaguideservice
+      .GetAreaGuide(this.AreaGuideObj)
+      .subscribe((result: any) => {
+        this.AreaGuideInterface = result;
+        this.IsLoaded = true;
+      });
   }
-  
-  FilterPropertiesByArea(area : number){
+
+  FilterPropertiesByArea(area: number) {
     this.router.navigate(['/property-list'], { queryParams: { area } });
   }
 }
